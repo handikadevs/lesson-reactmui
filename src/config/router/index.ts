eb4f8965d698de60883e7556a63527dfd31badad
Handikadevs <handikadevs@gmail.com>
2022-02-04 22:25:57 +0700

    first commit

---
+++ src/config/router/index.ts
@@ -0,0 +1,32 @@
+import MyComponent from "../../components/c1";
+
+export const router = [
+    {
+        icon: '',
+        label: 'Inbox',
+        path: '/inbox',
+        layout: 'dashboard',
+        component: MyComponent,
+    },
+    {
+        icon: '',
+        label: 'Mail',
+        path: '/mail',
+        layout: 'dashboard',
+        component: MyComponent,
+    },
+    {
+        icon: '',
+        label: 'Spam',
+        path: '/spam',
+        layout: 'dashboard',
+        component: MyComponent,
+    },
+    {
+        icon: '',
+        label: 'Trash',
+        path: '/trash',
+        layout: 'dashboard',
+        component: MyComponent,
+    },
+];
